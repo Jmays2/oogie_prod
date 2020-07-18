5798cd5264a91ccacddaf5981714562d29937b9a
System Administrator <root@MacBook-Pro.fios-router.home>
2020-07-18 01:33:03 -0400

    Updates

---
+++ precache-manifest.b92f8f9a915c3aa83d5fdacfcd0367c7.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06b282e72b1b671843056e39caf38e0d",
+    "url": "/oogie_prod/index.html"
+  },
+  {
+    "revision": "028ef21510bbd34dea5f",
+    "url": "/oogie_prod/static/css/main.19b9f121.chunk.css"
+  },
+  {
+    "revision": "3b87de0fa25af51ecb45",
+    "url": "/oogie_prod/static/js/2.4f920cb7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/oogie_prod/static/js/2.4f920cb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "028ef21510bbd34dea5f",
+    "url": "/oogie_prod/static/js/main.5835973e.chunk.js"
+  },
+  {
+    "revision": "0685c7637290afd7e0f8",
+    "url": "/oogie_prod/static/js/runtime-main.f3192b47.js"
+  },
+  {
+    "revision": "5b98a5e83ed7aba1daa3cabad8425cd8",
+    "url": "/oogie_prod/static/media/chefs.5b98a5e8.svg"
+  },
+  {
+    "revision": "0f04daad3a8bb0a8a8e0c3e3288fef1f",
+    "url": "/oogie_prod/static/media/chefs_long.0f04daad.svg"
+  },
+  {
+    "revision": "4b921bd91ab9e69f3616fb53a61f5d6d",
+    "url": "/oogie_prod/static/media/food_pic.4b921bd9.svg"
+  },
+  {
+    "revision": "c1914a221bd8ab4781c181c7264c0ee1",
+    "url": "/oogie_prod/static/media/hamburger-menu.c1914a22.svg"
+  },
+  {
+    "revision": "50deb56d0840f360f11fd0ce19d8b601",
+    "url": "/oogie_prod/static/media/oogie_logo.50deb56d.svg"
+  }
+]);
